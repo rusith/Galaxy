d8cd0cf8fdb0cf4446c4318efa0f971a778917ad
rusithbourketechnologies <rusith@bourketechnologies.com>
2016-08-02 15:21:49 +0530

    clean code

--- Galaxy/Extensions/BoolExtensions.cs
+++ Galaxy/Extensions/BoolExtensions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Galaxy.Extensions
+﻿namespace Galaxy.Extensions
 {
     /// <summary>
     /// extensions for bool
