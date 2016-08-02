99cbea1c33843f61620b04099003f0f1e7563b11
rusith <rusith@mail.com>
2016-08-02 07:33:18 +0530

    initial commit

---
+++ Galaxy/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Galaxy.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Format(this string str, params object[] parameters)
+        {
+            return !string.IsNullOrWhiteSpace(str) ? string.Format(str, parameters) : str;
+        }
+    }
+}
